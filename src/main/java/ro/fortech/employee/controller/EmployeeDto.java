c16094549570f7498a369e33b5e5f95e65e264e7
Daniel <daniel.danila@fortech.ro>
2020-02-11 15:57:23 +0200

    third

---
+++ src/main/java/ro/fortech/employee/controller/EmployeeDto.java
@@ -0,0 +1,58 @@
+package ro.fortech.employee.controller;
+
+import ro.fortech.employee.repository.EmployeeEntity;
+
+import java.util.Objects;
+
+public class EmployeeDto {
+    static String firstName;
+    static String lastName;
+
+    public EmployeeDto() {
+    }
+
+    public static EmployeeDto toDto(EmployeeEntity employeeEntity) {
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName(employeeEntity.getFirstName());
+        employeeDto.setLastName(employeeEntity.getLastName());
+        return employeeDto;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeDto that = (EmployeeDto) o;
+        return firstName.equals(that.firstName) &&
+                lastName.equals(that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
