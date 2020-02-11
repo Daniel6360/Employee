dda3ea5925af8f5ed0e412c1eb6fc48efa9a03b8
Daniel <daniel.danila@fortech.ro>
2020-02-11 16:02:43 +0200

    first post method

--- src/main/java/ro/fortech/employee/controller/EmployeeController.java
+++ src/main/java/ro/fortech/employee/controller/EmployeeController.java
@@ -1,4 +1,24 @@
 package ro.fortech.employee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ro.fortech.employee.repository.EmployeeEntity;
+import ro.fortech.employee.service.EmployeeService;
+
+@RestController
 public class EmployeeController {
+
+    EmployeeService service;
+
+    @Autowired
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/")
+    public void addEmployee(@RequestBody EmployeeEntity employeeEntity) {
+        service.addEmployee(employeeEntity);
+    }
 }
--- src/main/java/ro/fortech/employee/service/EmployeeService.java
+++ src/main/java/ro/fortech/employee/service/EmployeeService.java
@@ -1,4 +1,21 @@
 package ro.fortech.employee.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ro.fortech.employee.repository.EmployeeEntity;
+import ro.fortech.employee.repository.EmployeeRepository;
+
+@Service
 public class EmployeeService {
+    EmployeeRepository employeeRepository;
+
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+
+    public void addEmployee(EmployeeEntity employeeEntity) {
+        employeeRepository.save(employeeEntity);
+    }
 }
